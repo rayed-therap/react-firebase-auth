1ec3bbe905d5f046cc2fb1d471f9f39d049665e6
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-05-31 04:17:28 +0600

    Configure Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
