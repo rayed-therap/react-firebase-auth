b3a441a9f3bc67787db7f2c468f944cf19187264
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-05-31 16:48:26 +0600

    feat: Add SignUp form

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import SignUpForm from "./signup";
 
 const SignUpPage = () => {
-  return <div>Sign Up Page</div>;
+  const onSubmit: React.ChangeEventHandler<HTMLFormElement> = (event) => {
+    console.log(event);
+  };
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(event);
+  };
+
+  return (
+    <SignUpForm handleSubmit={onSubmit} handleChange={onChange}></SignUpForm>
+  );
 };
 
 export default SignUpPage;
