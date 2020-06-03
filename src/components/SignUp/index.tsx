6818134f93a58ae3d4fd1767019ee1599c9ffe68
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-06-03 15:47:25 +0600

    feat: Implement signup with Firebase

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,8 +1,48 @@
 import React, { useContext } from "react";
-import SignUpForm from "./signup";
+import { useHistory } from "react-router-dom";
 import { FirebaseContext } from "../Firebase";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import * as ROUTES from "../../constants/routes";
+import { Link as RouterLink } from "react-router-dom";
 
-const initialState = {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+type StateProps = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  error: any;
+};
+const INITIAL_STATE: StateProps = {
   username: "",
   email: "",
   password: "",
@@ -11,9 +51,8 @@ const initialState = {
 };
 
 const SignUpPage = () => {
-  let error = null;
-
-  const [state, setState] = React.useState(initialState);
+  const classes = useStyles();
+  const [state, setState] = React.useState(INITIAL_STATE);
 
   const isInvalid: boolean =
     state.username === "" ||
@@ -29,21 +68,124 @@ const SignUpPage = () => {
   };
 
   const firebase = useContext(FirebaseContext);
+  const history = useHistory();
 
   const onSubmit: React.ChangeEventHandler<HTMLFormElement> = (event) => {
-    const email = "email";
-    const password = "pass";
+    firebase
+      ?.doCreateUserWithEmailAndPassword(state.email, state.password)
+      .then((authUser) => {
+        setState({ ...INITIAL_STATE });
+        history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        setState({
+          ...state,
+          error: error,
+        });
+      });
 
-    firebase?.doCreateUserWithEmailAndPassword(email, password);
+    event.preventDefault();
   };
 
   return (
-    <SignUpForm
-      isInvalid={isInvalid}
-      handleSubmit={onSubmit}
-      handleChange={onChange}
-      error={error}
-    ></SignUpForm>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        {state.error && (
+          <Typography component="p" variant="body1">
+            {state.error.message}
+          </Typography>
+        )}
+        <form onSubmit={onSubmit} className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                onChange={onChange}
+                autoComplete="username"
+                name="username"
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="User Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={onChange}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={onChange}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={onChange}
+                variant="outlined"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                id="confirmPassword"
+                autoComplete="confirm-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={isInvalid}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link variant="body2" component={RouterLink} to={ROUTES.SIGN_IN}>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Copyright © "}
+          <Link color="inherit" component={RouterLink} to={ROUTES.LANDING}>
+            Sector18
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
