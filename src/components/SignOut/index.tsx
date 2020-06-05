0bbda5d7a4ebd0c4c9f58e225055ac28914fc897
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-06-05 11:59:02 +0600

    feature: Navigage to landing page after signout

--- src/components/SignOut/index.tsx
+++ src/components/SignOut/index.tsx
@@ -1,10 +1,23 @@
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { FirebaseContext } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 
 const SignOutButton = () => {
   const firebase = useContext(FirebaseContext);
+  const history = useHistory();
   return (
-    <button type="button" onClick={firebase?.doSignOut}>
+    <button
+      type="button"
+      onClick={() => {
+        firebase
+          ?.doSignOut()
+          .then(() => {
+            history.push(ROUTES.LANDING);
+          })
+          .catch((error) => console.log(error));
+      }}
+    >
       Sign Out
     </button>
   );
