8425e0bb0e2e02f477e00b980407da4ce5a9c17b
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-06-05 13:21:31 +0600

    feture: Make paths exact

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -19,19 +19,21 @@ const App = () => {
 
   return (
     <Router>
-      <div>
-        <Navigation authUser={authUser} />
-        <hr />
-        <Switch>
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-          <Route path={ROUTES.ADMIN} component={AdminPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        </Switch>
-      </div>
+      <Navigation authUser={authUser} />
+      <hr />
+      <Switch>
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route exact path={ROUTES.HOME} component={HomePage} />
+        <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+        <Route
+          exact
+          path={ROUTES.PASSWORD_FORGET}
+          component={PasswordForgetPage}
+        />
+      </Switch>
     </Router>
   );
 };
